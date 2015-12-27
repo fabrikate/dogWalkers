71b1845a0529670ecb2e9605f83660f11933c52b
KateStearns <katestearns@mac.com>
2015-12-27 13:43:41 -0800

    add schedulewalk angular routes

--- app/assets/javascripts/angular/app/scheduleWalk/scheduleWalk.controller.js
+++ app/assets/javascripts/angular/app/scheduleWalk/scheduleWalk.controller.js
@@ -1,11 +1,11 @@
-// (function() {
-//   angular
-//   .module('app.scheduleWalk')
-//   .controller('ScheduleWalkController', ScheduleWalkController);
-//
-//   ScheduleWalkController.$inject = ['$routeParams', 'UserFactory', 'DogFactory'];
-//
-//   function ScheduleWalkController($routeParams, UserFactory, DogFactory) {
-//     var vm = this;
-//   }
-// })();
+(function() {
+  angular
+  .module('app.scheduleWalk')
+  .controller('ScheduleWalkController', ScheduleWalkController);
+
+  ScheduleWalkController.$inject = ['$routeParams', 'UserFactory', 'DogFactory'];
+
+  function ScheduleWalkController($routeParams, UserFactory, DogFactory) {
+    var vm = this;
+  }
+})();
