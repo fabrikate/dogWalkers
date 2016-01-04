16601ddbe5097dbdf07d5352f6c77e4a0b379db9
KateStearns <katestearns@mac.com>
2016-01-03 16:17:09 -0800

    add walk overview page

--- app/assets/javascripts/angular/app/scheduleWalk/walk.controller.js
+++ app/assets/javascripts/angular/app/scheduleWalk/walk.controller.js
@@ -3,9 +3,39 @@
   .module('app.scheduleWalk')
   .controller('WalkController', WalkController)
 
-  WalkController.$inject = ['$routeParams', 'AppointmentFactory'];
+  WalkController.$inject = ['$routeParams', 'AppointmentFactory', 'UserFactory', 'DogFactory'];
 
-  function WalkController ($routeParams, AppointmentFactory) {
+  function WalkController ($routeParams, AppointmentFactory, UserFactory, DogFactory) {
+    var vm = this;
+    $('#landingPage').hide();
+    var userID = $routeParams.user_id;
+    var walkerID = $routeParams.walker_id;
+    var walkID = $routeParams.walk_id;
 
+    //variables to store walk information
+    vm.walk = AppointmentFactory.get({id: walkID}, function () {
+      vm.dogID = vm.walk.dog_id;
+      vm.dog = DogFactory.get({id: vm.dogID})
+      progressBar(vm.walk);
+    });
+    vm.owner = UserFactory.get({id: userID});
+    vm.walker = UserFactory.get({id: walkerID});
+    //function to show where the progress is in the walk appointment
+    function progressBar(apt) {
+      var headers = ['#step1', '#step2', '#step3', '#step4', '#step5'];
+      if ( apt.dogReturnedConfirm ) {
+        headers.forEach(function(step) {
+          $(step).attr('class', 'text-default');
+        });
+      } else if ( apt.walkerConfirm ) {
+        for ( var i = 0; i < 2; i++ ) {
+          $(headers[i]).attr('class', 'text-default');
+        }
+      } else if ( apt.ownerRequested ) {
+        $('#step1').attr('class', 'text-default');
+      } else {
+        console.log('error!');
+      }
+    }
   }
 })();
