aedc0aed57e52cb8df003b9c4591c838df6599f1
KateStearns <katestearns@mac.com>
2016-01-12 09:52:27 -0800

    bug fix: crash on heroku error h10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: '/auth/facebook'
+  root to: 'home#show'
 
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: redirect('/')
